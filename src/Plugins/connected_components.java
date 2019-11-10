b216b4aae98acd811cb7c8dc32da88c2a6c79dd9
Sam Steinberg <ssteinb2@vols.utk.edu>
2019-11-10 01:30:32 -0500

    finished connected components

--- src/Plugins/connected_components.java
+++ src/Plugins/connected_components.java
@@ -4,7 +4,9 @@ import ij.gui.DialogListener;
 import ij.gui.GenericDialog;
 import ij.gui.Overlay;
 import ij.plugin.OverlayCommands;
+import ij.plugin.OverlayLabels;
 import ij.plugin.filter.*;
+import ij.plugin.tool.OverlayBrushTool;
 import ij.process.*;
 
 import java.util.*;
@@ -28,6 +30,7 @@ public class connected_components implements ExtendedPlugInFilter, DialogListene
           "Labels", "BBOX" };
   private String labelOption = outputLabelText[0];
   private int[][] label;
+  TreeMap<Integer,Integer> setLabel = new TreeMap<Integer,Integer>();
 
   @Override
   public int setup(String arg, ImagePlus imp) {
@@ -37,20 +40,20 @@ public class connected_components implements ExtendedPlugInFilter, DialogListene
   @Override
   public void run(ImageProcessor ip) {
 
-    // Assign labels to object pixels (255)
-
 
     if (labelOption.equals(outputLabelText[0])){
       // Binarize image using default method
       ip.autoThreshold();
       assignLabels(ip);
+      labelDiscards(ip);
     }
+    /* BBOX option...get labels but use a temp image. If contrast stretch selected perform here. */
     else{
       ImageProcessor I0 = ip.duplicate();
       I0.autoThreshold();
       assignLabels(I0);
+      if (contrast_stretch) contrast_stretch(ip);
       overlayBBOX(ip);
-      //ip.insert(ip.convertToByte(true), 0, 0);
     }
 
   }
@@ -92,23 +95,17 @@ public class connected_components implements ExtendedPlugInFilter, DialogListene
     ;
   }
 
-  private void overlayBBOX(ImageProcessor ip){
-    //if overlays for labels become too much make visited list
-    //make a size list for each pixel (u,v) = i
-    //add condition here like if(border) dont add these else add all
-    ColorProcessor ip_overlay = ip.convertToColorProcessor();
-    ip_overlay.setColor(Color.yellow);
+  /* Object discards for "Labels" option. Labels added to arraylist dont fir criteria and are whitened out */
+  private void labelDiscards(ImageProcessor ip){
     int min_u, max_u, min_v, max_v, size_sum;
-    ArrayList<Integer> objSize = new ArrayList<Integer>();
-
-    for (int i: disjointSetLabel){
-      if (i != -1){
+    ArrayList<Integer> hs = new ArrayList<Integer>();
+    for (int i = 1; i <= Nobjects; i++){
         min_u = Integer.MAX_VALUE; max_u = -1;
         min_v = Integer.MAX_VALUE; max_v = -1;
         size_sum = 0;
-        for (int v = 0; v < ip.getHeight(); v++){
-          for (int u = 0; u < ip.getWidth(); u++){
-            if (label[u][v] == i){
+        for (int v = 0; v < ip.getHeight(); v++) {
+          for (int u = 0; u < ip.getWidth(); u++) {
+            if (setLabel.get(findLabel(label[u][v])) == i) {
               size_sum++;
               if (u < min_u) min_u = u;
               if (u > max_u) max_u = u;
@@ -117,24 +114,132 @@ public class connected_components implements ExtendedPlugInFilter, DialogListene
             }
           }
         }
-        objSize.add(size_sum);
-        /* If border option is on, look for min coords that are out of bounds */
+      processOptions(min_u, max_u, min_v, max_v, size_sum, ip,hs, i);
+
+    }
+    for (int j: hs){
+      for (int v = 0; v < ip.getHeight(); v++) {
+        for (int u = 0; u < ip.getWidth(); u++) {
+          if (setLabel.get(findLabel(label[u][v])) == j) {
+            ip.set(u, v, 0);
+          }
+        }
+      }
+    }
+  }
+  /* Criteria to be erased... */
+  private void processOptions(int min_u, int max_u, int min_v, int max_v, int size_sum, ImageProcessor ip, ArrayList<Integer> hs, int i){
+    //If border option is on, look for min coords that are out of bounds
+    if (border) {
+      if (min_u <= 0 || max_u >= (ip.getWidth() - 1) || min_v <= 0 || max_v >= (ip.getHeight()-1)) {
+        hs.add(i);
+      }
+    }
+    // If NOT between min and max do not apply a box, continue to next iteration.
+    if ( !(min <= size_sum && max >= size_sum)){
+      hs.add(i);
+    }
+    if (noncircular) {
+      if (circleCheck((max_u - min_u), (max_v - min_v))) {
+        hs.add(i);
+      }
+    }
+  }
+  /* Check for circular vs non circular objs */
+  private boolean circleCheck(int width, int height){
+    if ( (Math.abs(width - height) > 0.2 * width) || Math.abs(width - height) > 0.2 * height){
+      return true;
+    }
+    double a_circle = Math.PI * 1 / 4 * width * height;
+    double a_square = width * height;
+    if ((Math.abs(a_square - a_circle) < 0.2 * a_circle) || (Math.abs(a_square - a_circle) < 0.2 * a_square)){
+      return true;
+    }
+    return false;
+  }
+
+  /*Overlaying BBOX option */
+  private void overlayBBOX(ImageProcessor ip){
+    ip.setColor(Color.white);
+    int min_u, max_u, min_v, max_v, size_sum, width, height;
+  /* go through each label and find bounding area */
+    for (int i = 1; i <= Nobjects; i++){
+        min_u = Integer.MAX_VALUE; max_u = -1;
+        min_v = Integer.MAX_VALUE; max_v = -1;
+        size_sum = 0;
+        for (int v = 0; v < ip.getHeight(); v++) {
+          for (int u = 0; u < ip.getWidth(); u++) {
+            if (setLabel.get(findLabel(label[u][v])) == i) {
+              size_sum++;
+              if (u < min_u) min_u = u;
+              if (u > max_u) max_u = u;
+              if (v < min_v) min_v = v;
+              if (v > max_v) max_v = v;
+            }
+          }
+        }
+        //If border option is on, look for min coords that are out of bounds
         if (border) {
           if (min_u <= 0 || max_u >= (ip.getWidth() - 1) || min_v <= 0 || max_v >= (ip.getHeight()-1)) {
             continue;
           }
         }
-        /* If NOT between min and max do not apply a box, continue to next iteration. */
-        if ( !(min <= size_sum && max >= size_sum)){
+        // If NOT between min and max do not apply a box, continue to next iteration.
+        if ( !(min < size_sum && max > size_sum)){
           continue;
         }
-        ip_overlay.drawRect(min_u, min_v, (max_u-min_u), (max_v-min_v));
-    }
-    }
-    for (int z : disjointSetRank) IJ.log(String.format("size at index is: %d\n", z));
-    ImagePlus imp_overlay = new ImagePlus("blobs.gif", ip_overlay);
-    imp_overlay.show();
 
+        /* Formulas for non-circular objects */
+        if (noncircular) {
+          if (circleCheck((max_u - min_u), (max_v - min_v))) {
+            continue;
+          }
+        }
+        ip.drawRect(min_u, min_v, (max_u-min_u), (max_v-min_v));
+    }
+
+  }
+  /* Used contrast stretch plugin from earlier this semester */
+  private void contrast_stretch(ImageProcessor ip){
+
+      int[] H = ip.getHistogram(256);
+
+      int _sum = 0;
+      for (int r=1; r<H.length; r++){
+        _sum += H[r];
+      }
+      int clamp = (int)(Math.floor(_sum*0.02));
+      int c_sum = 0, r1 = 0, r2 = 0;
+      for (int r = 0; r < 256; r++){
+        c_sum += H[r];
+        if (c_sum >= clamp){
+          r1 = r;
+          break;
+        }
+      }
+      c_sum = 0;
+      for (int r = 255; r >= 0; r--){
+        c_sum += H[r];
+        if (c_sum >= clamp){
+          r2 = r;
+          break;
+        }
+      }
+      int[] I = new int[256];
+      for (int r = 0; r < 256; r++){
+        if (r < r1){
+          I[r] = 0;
+        }
+        else if (r2 <= r){
+          I[r] = 255;
+        }
+        else{
+          double ex1 = (double)(r-r1);
+          double ex2 = (double)(r2-r1);
+          I[r] = (int)Math.floor(255*ex1/ex2);
+        }
+      }
+      ip.applyTable(I);
   }
 
   private void assignLabels(ImageProcessor ip) {
@@ -188,7 +293,6 @@ public class connected_components implements ExtendedPlugInFilter, DialogListene
 
     // Convert labels to sequence 1:N
 
-    TreeMap<Integer,Integer> setLabel = new TreeMap<Integer,Integer>();
 
     setLabel.put(0, 0);
 
